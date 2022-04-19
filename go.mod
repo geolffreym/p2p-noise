472145b50b3cd75f90526fc41467cd7e982aeeae
geolffreym <gmjun2000@gmail.com>
2022-04-19 13:20:48 -0600

    first commit

---
+++ go.mod
@@ -0,0 +1,4 @@
+module github.com/geolffreym/p2p
+
+go 1.18
+
---
+++ main.go
@@ -0,0 +1,8 @@
+package p2p
+
+import "github.com/geolffreym/p2p-discovery/networking"
+
+func main() {
+	net := networking.Network{}
+
+}
