9c42e5e8a9a77cc9b5b626ce5b0439849d4b2070
geolffreym <gmjun2000@gmail.com>
2022-05-09 13:06:24 -0600

    docs: peer network

--- network/peer.go
+++ network/peer.go
@@ -2,13 +2,31 @@ package network
 
 import "net"
 
+/*
+Node peer definition
+
+ socket:
+ 	IP and Port address for peer.
+	https://en.wikipedia.org/wiki/Network_socket
+ conn:
+ 	Connection interface net.Conn to reach peer.
+*/
 type Peer struct {
 	socket Socket
 	conn   net.Conn
 }
 
-func (r *Peer) Connection() net.Conn                 { return r.conn }
-func (r *Peer) Socket() Socket                       { return r.socket }
+// Return peer connection interface
+func (r *Peer) Connection() net.Conn { return r.conn }
+
+// Return peer socket
+func (r *Peer) Socket() Socket { return r.socket }
+
+// Write buffered message over connection
 func (r *Peer) Write(data []byte) (n int, err error) { return r.conn.Write(data) }
-func (r *Peer) Read(buf []byte) (n int, err error)   { return r.conn.Read(buf) }
-func (r *Peer) Close() error                         { return r.conn.Close() }
+
+// Read buffered message from connection
+func (r *Peer) Read(buf []byte) (n int, err error) { return r.conn.Read(buf) }
+
+// Close peer connection
+func (r *Peer) Close() error { return r.conn.Close() }
--- network/router.go
+++ network/router.go
@@ -1,19 +1,18 @@
 package network
 
-// Aliases to handle idiomatic `string` type
+// Aliases to handle idiomatic `Socket` type
 type Socket string
 
 // Router hash table to associate Socket with connection interfaces.
-// eg.
-// {127.0.0.1:4000: net.Conn}
+// eg. {127.0.0.1:4000: net.Conn}
 type Router map[Socket]*Peer
 
-// Add new socket => connection assoc
+// Add new socket => connection association
 func (r Router) Add(socket Socket, route *Peer) {
 	r[socket] = route
 }
 
-// Return the number of connections associated
+// Return the number of connections
 func (r Router) Len() int {
 	return len(r)
 }
