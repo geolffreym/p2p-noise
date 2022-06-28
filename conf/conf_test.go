d7ba81e49dcf2a08b5548a74fa680e61aeeeecba
geolffreym <gmjun2000@gmail.com>
2022-06-28 12:38:18 -0600

    feat: functional options

---
+++ conf/conf_test.go
@@ -0,0 +1,57 @@
+package conf
+
+import (
+	"testing"
+)
+
+func TestWrite(t *testing.T) {
+
+	settings := []struct {
+		Name              string
+		MaxPeersConnected uint8
+	}{{
+		Name:              "10",
+		MaxPeersConnected: 10,
+	}, {
+		Name:              "20",
+		MaxPeersConnected: 20,
+	}, {
+		Name:              "30",
+		MaxPeersConnected: 30,
+	}, {
+		Name:              "60",
+		MaxPeersConnected: 60,
+	}, {
+		Name:              "255",
+		MaxPeersConnected: 255,
+	}}
+
+	myLib := func(c ...Setting) *Settings {
+		s := NewSettings()
+		s.Write(c...)
+		return s
+	}
+
+	for _, e := range settings {
+		t.Run(e.Name, func(t *testing.T) {
+			libWithSettings := myLib(SetMaxPeersConnected(e.MaxPeersConnected))
+
+			if libWithSettings.MaxPeersConnected != e.MaxPeersConnected {
+				t.Errorf("expected MaxPeerConnected %#v, get settings %v", e.MaxPeersConnected, libWithSettings.MaxPeersConnected)
+			}
+
+		})
+
+	}
+
+}
+
+func TestSetMaxPeersConnected(t *testing.T) {
+	settings := NewSettings()
+	callable := SetMaxPeersConnected(10)
+	callable(settings)
+
+	if settings.MaxPeersConnected != 10 {
+		t.Errorf("expected MaxPeerConnected %#v, get settings %v", 10, settings.MaxPeersConnected)
+	}
+}
---
+++ utils/utils_test.go
@@ -0,0 +1,62 @@
+package utils
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestUtilClear(t *testing.T) {
+
+	type S struct {
+		a int
+		b int
+		c []byte
+	}
+
+	a := []byte("abcdef")
+	b := []int{1, 2, 3, 4, 5, 6}
+	c := S{
+		a: 1,
+		b: 2,
+		c: a,
+	}
+
+	Clear(&a)
+	Clear(&b)
+	Clear(&c)
+
+	if len(a) > 0 || len(b) > 0 {
+		t.Errorf("Expected empty slices after clearing")
+	}
+
+	if (!reflect.DeepEqual(c, S{})) {
+		t.Errorf("Expected empty struct after clearing")
+	}
+
+}
+
+func TestIndexOf(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 6, 7, 8, 9}
+	// Table driven test
+	// For each expected event
+	for _, e := range slice {
+		t.Run(fmt.Sprintf("Match for %x", e), func(t *testing.T) {
+			match := IndexOf(slice, e)
+			if ^match == 0 {
+				t.Errorf("expected matched existing elements in slice index: %#v", e)
+			}
+		})
+
+	}
+}
+
+func TestInvalidIndexOf(t *testing.T) {
+	slice := []int{1, 2, 3, 4, 6, 7, 8, 9}
+	match := IndexOf(slice, 5)
+
+	if ^match != 0 {
+		t.Error("Number 5 is not in slice cannot be found")
+	}
+
+}
