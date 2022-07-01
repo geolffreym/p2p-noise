8aba1a89e47aa6c77f9d2cb9c426d918fbc0ec5d
geolffreym <gmjun2000@gmail.com>
2022-07-01 16:29:18 -0600

    test: subscribers

--- subscriber_test.go
+++ subscriber_test.go
@@ -1,1 +1,49 @@
 package noise
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestListen(t *testing.T) {
+	sub := newSubscriber()
+
+	message := Message{
+		SelfListening,
+		[]byte("hello test 1"),
+	}
+
+	canceled := make(chan struct{})
+	msg := make(chan Message)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		// wait for new message emitted then cancel listening
+		<-msg
+		cancel()
+	}()
+
+	go func() {
+		// after stop listening loop expect trigger canceled
+		sub.Listen(ctx, msg)
+		canceled <- struct{}{}
+	}()
+
+	go func() {
+		// send message after 1 second
+		time.Sleep(1 * time.Second)
+		sub.Emit(message)
+	}()
+
+	// First to finish wins
+	select {
+	case <-canceled:
+		t.Log("canceled channel")
+	case <-time.After(3 * time.Second):
+		// Wait 1 second to receive message
+		t.Errorf("expected canceled listening after emit")
+		t.FailNow()
+	}
+
+}
