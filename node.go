12592a1bb4f2c14a9f2a136cd851c2567077dab3
geolffreym <gmjun2000@gmail.com>
2022-07-23 13:16:54 -0600

    feat: dynamic buffer allocation

--- node.go
+++ node.go
@@ -4,7 +4,10 @@
 //See also: http://www.noiseprotocol.org/noise.html#introduction
 package noise
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Config interface {
 	MaxPeersConnected() uint8
@@ -12,6 +15,54 @@ type Config interface {
 }
 
 type Node struct {
-	net   *Network
-	noise *noise
+	//monitor
+	net *network // networking
+	// sec *Noise   // security
+
+}
+
+func New(config Config) *Node {
+	return &Node{
+		newNetwork(config),
+	}
+}
+
+// Events its a middleware/proxy to intercept and pre-process raw incoming messages
+func (n *Node) Events(ctx context.Context) <-chan Message {
+	events := n.net.Events(ctx)
+	ch := make(chan Message)
+
+	// Intercept messages to split, public from internal events
+	go func() {
+		for msg := range events {
+			// Here could be handled events
+			if msg.Type() == NewPeerDetected {
+				// TODO start handshake?
+				// cancel() // stop listening for events
+
+			}
+
+			if msg.Type() == MessageReceived {
+				// Decrypt message
+			}
+
+			// Redirect message
+			ch <- msg
+		}
+	}()
+
+	return ch
+}
+
+func (n *Node) SendMessage() {
+	// Encrypt message
+}
+
+func (n *Node) Handshake() {
+
+}
+
+// Listen forward method using default node address
+func (n *Node) Listen(addr Socket) error {
+	return n.net.Listen(addr)
 }
--- noise.go
+++ noise.go
@@ -1,6 +1,6 @@
 package noise
 
-type noise struct {
+type Noise struct {
 }
 
 // handshake
