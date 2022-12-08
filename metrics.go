be5ebd407dae15d354870f1afe0304b5ba55f56f
geolffreym <gmjun2000@gmail.com>
2022-12-08 10:06:48 -0600

    feat: metrics draft

--- metrics.go
+++ metrics.go
@@ -3,8 +3,8 @@ package noise
 // metrics hold the statistics related to remote peers.
 // We can add any method related to adaptive lookup logic here.
 type metrics struct {
-	handshakeStart uint64
-	handshakeEnd   uint64
+	handshakeStart uint32
+	handshakeEnd   uint32
 	latency        uint16
 	bandwidth      uint16
 }
